726867ed485bd14c35e84fde8056aefd87794719
unknown <namoda378@gmail.com>
2019-02-27 15:41:21 +0900

    hansung modified 0227

--- e__network/a__receiver.lua
+++ e__network/a__receiver.lua
@@ -2,11 +2,50 @@ return function(p,o,c)
 	-- body
 
 
+	local t3_model_lib
+
+	local receive_ = {}
+
+
+	receive_.model.t3 
+
+
+
 	o.func = function(response)
 
 		dump_table(response,-10)
 
-		
+		for i,packet in pairs(response) do 
+
+			local reqnames = packet.reqnames
+
+			-- putstone response doesn't need to be processed
+
+			if reqnames[0] == 'model' then 
+
+				if reqnames[0] == 't3' then 
+
+					local colubrow = packet.colubrow
+					local t3_model = t3_model_lib[colubrow]
+
+					t3_model:sync(packet)
+
+
+				elseif reqnames[0] == 't2' then 
+
+
+
+				end
+
+
+			elseif reqnames[0] == 'model' then 
+
+
+			end
+
+
+
+		end
 	end
 
 
